a7fe47f9c4b9e57226c49bb9db80fa8823abeaae
Edgar Ruiz <edgararuiz@gmail.com>
2023-04-12 12:50:06 -0500

    look refinements

--- inst/apps/chat.R
+++ inst/apps/chat.R
@@ -1,15 +1,21 @@
+library(rclipboard)
+
 ui <- fluidPage(
+  rclipboardSetup(),
   fixedPanel(
     width = "100%", left = -1,
     fluidRow(
       column(width = 1, div()),
-      column(width = 9, textAreaInput("prompt", "", width = "90%", value = "test", resize = "none")),
+      column(
+        width = 9,
+        textAreaInput("prompt", "", width = "70%", value = "test", resize = "horizontal")
+      ),
       column(width = 2, br(), actionButton("add", "Submit"))
     ),
-    style = "opacity: 1; z-index: 10; background-color: #099;"
+    style = "opacity: 1; z-index: 10; background-color: #0a9;"
   ),
   absolutePanel(
-    top = 100, width = "100%",
+    top = 100, width = "95%",
     tabsetPanel(
       type = "tabs",
       id = "tabs"
@@ -18,15 +24,50 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  # Add clipboard buttons
+  # output$clip <- renderUI({
+  output$clip <- renderUI({
+    rclipButton(
+      inputId = "clipbtn",
+      label = "Copy",
+      clipText = input$copytext,
+      icon = icon("clipboard")
+    )
+  })
+  # })
+
   observeEvent(input$add, {
     insertUI(
       selector = "#tabs",
       where = "afterEnd",
       ui = {
-        wellPanel(renderText(input$prompt))
+
+        fluidRow(
+          style = "
+          border-color: #ddd;
+          border-style: solid;
+          background-color: #fff;
+          border-width: 1px;
+          padding-left: 10px;
+          padding-right: 10px;
+          margin: 10px;
+          ",
+          markdown("
+    # Markdown Example
+
+    This is a markdown paragraph, and will be contained within a `<p>` tag
+    in the UI.
+
+    ```{r}
+    1 + 1
+    ```
+    ")
+        )
       }
     )
-  })
+  }
+
+  )
 }
 
 shinyApp(ui, server)
