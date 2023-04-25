d1c6f6a94e3c6947cebf8220681d87cef72029f2
Edgar Ruiz <edgararuiz@gmail.com>
2023-04-25 17:25:28 -0500

    Changes how yaml file is built

--- R/tidychat.R
+++ R/tidychat.R
@@ -1,10 +1,29 @@
 # --------------------------------- YAML ---------------------------------------
-
+#' Creates
 #' @param overwrite If there's an existing "config.yml", should it be replaced?
 #' @export
-tidychat_yaml_copy <- function(overwrite = FALSE) {
-  from <- system.file("configs/gpt3.5.yml", package = "tidychat")
-  fs::file_copy(from, "config.yml", overwrite = overwrite)
+tidychat_yaml_copy <- function(path = "config.yml",
+                               overwrite = FALSE
+                               ) {
+
+  notebook <- tidychat_defaults(type = "notebook")
+  notebook$prompt <- paste0(notebook$prompt, collapse = "\n")
+
+  chat <- tidychat_defaults(type = "chat")
+  chat$prompt <- paste0(chat$prompt, collapse = "\n")
+
+  temp <- tempfile()
+  x <- list(
+    default = list(
+      tidychat = list(
+        notebook = notebook,
+        chat = chat
+      )
+    )
+  )
+
+  yaml::write_yaml(x = x, file = temp)
+  fs::file_copy(temp, path, overwrite = overwrite)
 }
 
 # -------------------------------- History -------------------------------------
