71f714cfaac67df80df1024d2f64154b8b42170c
Edgar Ruiz <edgararuiz@gmail.com>
2023-05-03 11:25:19 -0500

    Organizes openai functions

--- R/openai-core.R
+++ R/openai-core.R
@@ -1,84 +1,3 @@
-openai_get_completion_text <- function(prompt = NULL,
-                                       model = "text-davinci-003",
-                                       defaults = NULL,
-                                       r_file_stream = NULL,
-                                       r_file_complete = NULL) {
-  req_body <- c(
-    list(
-      model = model,
-      prompt = prompt
-    ),
-    defaults$model_arguments
-  )
-
-  ret <- openai_switch(
-    endpoint = "completions",
-    req_body = req_body,
-    defaults = defaults,
-    r_file_stream = r_file_stream,
-    r_file_complete = r_file_complete
-    )
-
-  if(inherits(ret, "list")) {
-    ret <- ret$choices[[1]]$text
-  }
-
-  ret
-}
-
-openai_get_chat_completion_text <- function(prompt = NULL,
-                                            model = "gpt-3.5-turbo",
-                                            defaults = NULL,
-                                            r_file_stream = NULL,
-                                            r_file_complete = NULL) {
-  req_body <- c(
-    list(
-      model = model,
-      messages = prompt
-    ),
-    defaults$model_arguments
-  )
-
-  ret <- openai_switch(
-    endpoint = "chat/completions",
-    req_body = req_body,
-    defaults = defaults,
-    r_file_stream = r_file_stream,
-    r_file_complete = r_file_complete
-    )
-
-  if(inherits(ret, "list")) {
-    ret <- ret$choices[[1]]$message$content
-  }
-
-  ret
-}
-
-openai_switch <- function(endpoint,
-                          req_body,
-                          defaults,
-                          r_file_stream,
-                          r_file_complete
-                          ) {
-  ret <- NULL
-  stream <- defaults$model_arguments$stream %||% FALSE
-  if (stream) {
-    if(defaults$type == "chat") {
-      ret <- openai_stream_file(
-        endpoint = endpoint,
-        req_body = req_body,
-        r_file_stream = r_file_stream,
-        r_file_complete = r_file_complete
-        )
-    } else {
-      openai_stream_ide(endpoint, req_body)
-    }
-  } else {
-    ret <- openai_perform(endpoint, req_body)
-  }
-  ret
-}
-
 openai_perform <- function(endpoint, req_body) {
   if (tidychat_debug_get()) {
     req_body
@@ -111,4 +30,106 @@ openai_token <- function() {
   }
 
   ret
+}
+
+openai_stream_ide <- function(endpoint, req_body) {
+  if (tidychat_debug_get()) {
+    req_body
+  } else {
+    ide_paste_text("\n\n")
+    openai_request(endpoint, req_body) %>%
+      req_stream(
+        function(x) {
+          tidychat_env$stream$raw <- paste0(
+            tidychat_env$stream$raw,
+            rawToChar(x),
+            collapse = ""
+          )
+          current <- openai_stream_parse(
+            x = tidychat_env$stream$raw,
+            endpoint = endpoint
+          )
+          if(!is.null(current)) {
+            if(is.null(tidychat_env$stream$response)) {
+              ide_paste_text(current)
+            } else {
+              if(nchar(current) != nchar(tidychat_env$stream$response)) {
+                delta <- substr(
+                  current,
+                  nchar(tidychat_env$stream$response) + 1,
+                  nchar(current)
+                )
+                ide_paste_text(delta)
+              }
+            }
+          }
+          tidychat_env$stream$response <- current
+          TRUE
+        },
+        buffer_kb = 0.1
+      )
+    ide_paste_text("\n\n")
+  }
+}
+
+openai_stream_file <- function(endpoint,
+                               req_body,
+                               r_file_stream,
+                               r_file_complete
+) {
+  if (tidychat_debug_get()) {
+    req_body
+  } else {
+
+    tidychat_env$stream$response <- NULL
+
+    openai_request(endpoint, req_body) %>%
+      req_stream(
+        function(x) {
+          tidychat_env$stream$response <- paste0(
+            tidychat_env$stream$response,
+            rawToChar(x),
+            collapse = ""
+          )
+          ret <- openai_stream_parse(
+            x = tidychat_env$stream$response,
+            endpoint = endpoint
+          )
+          saveRDS(ret, r_file_stream)
+          TRUE
+        },
+        buffer_kb = 0.1
+      )
+    ret <- readRDS(r_file_stream)
+    file_delete(r_file_stream)
+    saveRDS(ret, r_file_complete)
+    ret
+  }
+}
+
+openai_stream_parse <- function(x, endpoint) {
+  res <- x %>%
+    paste0(collapse = "") %>%
+    strsplit("data: ") %>%
+    unlist() %>%
+    purrr::discard(~ .x == "")  %>%
+    purrr::keep(~ substr(.x, (nchar(.x) - 2), nchar(.x)) == "}\n\n") %>%
+    map(jsonlite::fromJSON)
+
+  if(length(res) > 0) {
+
+    if(endpoint == "completions") {
+      res <- res %>%
+        map(~ .x$choices$text) %>%
+        reduce(paste0)
+    }
+
+    if(endpoint == "chat/completions") {
+      res <- res %>%
+        map(~ .x$choices$delta$content) %>%
+        reduce(paste0)
+    }
+
+    if(length(res) > 0) return(res)
+  }
 }
--- R/openai-submit.R
+++ R/openai-submit.R
@@ -16,7 +16,7 @@ tidychat_submit.tc_provider_open_ai  <- function(defaults,
   if(preview) {
     ret <- prompt
   } else {
-    ret <- gpt_completion(
+    ret <- openai_completion(
       defaults = defaults,
       prompt = prompt,
       r_file_stream = r_file_stream,
@@ -29,15 +29,15 @@ tidychat_submit.tc_provider_open_ai  <- function(defaults,
   ret
 }
 
-gpt_completion <- function(defaults, prompt, r_file_stream, r_file_complete) {
-  UseMethod("gpt_completion")
+openai_completion <- function(defaults, prompt, r_file_stream, r_file_complete) {
+  UseMethod("openai_completion")
 }
 
-gpt_completion.tc_model_gpt_3.5_turbo <- function(defaults,
-                                                  prompt,
-                                                  r_file_stream,
-                                                  r_file_complete
-                                                  ) {
+openai_completion.tc_model_gpt_3.5_turbo <- function(defaults,
+                                                     prompt,
+                                                     r_file_stream,
+                                                     r_file_complete
+) {
   openai_get_chat_completion_text(
     prompt = prompt,
     model = "gpt-3.5-turbo",
@@ -47,11 +47,11 @@ gpt_completion.tc_model_gpt_3.5_turbo <- function(defaults,
   )
 }
 
-gpt_completion.tc_model_davinci_3  <- function(defaults,
-                                               prompt,
-                                               r_file_stream,
-                                               r_file_complete
-                                               ) {
+openai_completion.tc_model_davinci_3  <- function(defaults,
+                                                  prompt,
+                                                  r_file_stream,
+                                                  r_file_complete
+) {
   openai_get_completion_text(
     prompt = prompt,
     model = "text-davinci-003",
@@ -59,4 +59,85 @@ gpt_completion.tc_model_davinci_3  <- function(defaults,
     r_file_stream = r_file_stream,
     r_file_complete = r_file_complete
   )
+}
+
+openai_get_chat_completion_text <- function(prompt = NULL,
+                                            model = "gpt-3.5-turbo",
+                                            defaults = NULL,
+                                            r_file_stream = NULL,
+                                            r_file_complete = NULL) {
+  req_body <- c(
+    list(
+      model = model,
+      messages = prompt
+    ),
+    defaults$model_arguments
+  )
+
+  ret <- openai_switch(
+    endpoint = "chat/completions",
+    req_body = req_body,
+    defaults = defaults,
+    r_file_stream = r_file_stream,
+    r_file_complete = r_file_complete
+  )
+
+  if(inherits(ret, "list")) {
+    ret <- ret$choices[[1]]$message$content
+  }
+
+  ret
+}
+
+openai_get_completion_text <- function(prompt = NULL,
+                                       model = "text-davinci-003",
+                                       defaults = NULL,
+                                       r_file_stream = NULL,
+                                       r_file_complete = NULL) {
+  req_body <- c(
+    list(
+      model = model,
+      prompt = prompt
+    ),
+    defaults$model_arguments
+  )
+
+  ret <- openai_switch(
+    endpoint = "completions",
+    req_body = req_body,
+    defaults = defaults,
+    r_file_stream = r_file_stream,
+    r_file_complete = r_file_complete
+  )
+
+  if(inherits(ret, "list")) {
+    ret <- ret$choices[[1]]$text
+  }
+
+  ret
+}
+
+openai_switch <- function(endpoint,
+                          req_body,
+                          defaults,
+                          r_file_stream,
+                          r_file_complete
+) {
+  ret <- NULL
+  stream <- defaults$model_arguments$stream %||% FALSE
+  if (stream) {
+    if(defaults$type == "chat") {
+      ret <- openai_stream_file(
+        endpoint = endpoint,
+        req_body = req_body,
+        r_file_stream = r_file_stream,
+        r_file_complete = r_file_complete
+      )
+    } else {
+      openai_stream_ide(endpoint, req_body)
+    }
+  } else {
+    ret <- openai_perform(endpoint, req_body)
+  }
+  ret
 }
