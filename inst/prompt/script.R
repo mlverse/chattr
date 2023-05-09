67cb24438b54845faac8cad830dbaa3939e94401
Edgar Ruiz <edgararuiz@gmail.com>
2023-05-09 09:42:21 -0500

    Removes test qmds, moves base prompt to inst so it's not a package dataset any longer

--- inst/prompt/script.R
+++ inst/prompt/script.R
@@ -1,17 +1,4 @@
-#' Recommended base prompt
-#' @format Character vectors
-#' @description It is meant to be used within the tidychat_default() function
-#' @examples
-#'
-#' tc_defaults(
-#'   c("This is an example", "{tc_base_prompt}"),
-#'   type = "chat"
-#' )
-#'
-"tc_base_prompt"
-
-
-tidychat_base <- function() {
+writeLines(
   c(
     "Use the 'Tidy Modeling with R' (https://www.tmwr.org/) book as main reference",
     "Use the 'R for Data Science' (https://r4ds.had.co.nz/) book as main reference",
@@ -19,7 +6,8 @@ tidychat_base <- function() {
     "skimr and janitor can also be used if needed",
     "For models, use tidymodels packages: recipes, parsnip, yardstick, workflows, broom",
     "Expecting only code, avoid comments unless requested by user"
-  )
-}
-# tc_base_prompt <- tidychat_base()
-# usethis::use_data(tc_base_prompt)
+  ),
+  here::here("inst", "prompt", "base.txt")
+)
+
+
