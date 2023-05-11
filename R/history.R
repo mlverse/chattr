8abb814c264c9dbd39328d929156e7ae498fa11a
Edgar Ruiz <edgararuiz@gmail.com>
2023-05-11 10:12:24 -0500

    Fixes chat history in app

---
+++ R/history.R
@@ -0,0 +1,30 @@
+#' Displays the current session' chat history
+#' @param x An list object that contains chat history. Use this argument to
+#' override the current history.
+#' @export
+tc_history <- function(x = NULL) {
+  if(!is.null(x)) {
+    tc_env$chat_history <- x
+  }
+  tc_env$chat_history
+}
+
+tc_history_append <- function(user = NULL, assistant = NULL) {
+  if (!is.null(user)) {
+    tc_env$chat_history <- c(
+      tc_env$chat_history,
+      list(list(role = "user", content = user))
+    )
+  }
+
+  if (!is.null(assistant)) {
+    tc_env$chat_history <- c(
+      tc_env$chat_history,
+      list(list(role = "assistant", content = assistant))
+    )
+  }
+}
+
+tc_history_set <- function(x) {
+  tc_env$chat_history <- x
+}
--- R/utils.R
+++ R/utils.R
@@ -1,29 +1,3 @@
-# -------------------------------- History -------------------------------------
-
-tc_history_get <- function() {
-  tc_env$chat_history
-}
-
-tc_history_append <- function(user = NULL, assistant = NULL) {
-  if (!is.null(user)) {
-    tc_env$chat_history <- c(
-      tc_env$chat_history,
-      list(list(role = "user", content = user))
-    )
-  }
-
-  if (!is.null(assistant)) {
-    tc_env$chat_history <- c(
-      tc_env$chat_history,
-      list(list(role = "assistant", content = assistant))
-    )
-  }
-}
-
-tc_history_set <- function(x) {
-  tc_env$chat_history <- x
-}
-
 # --------------------------------- Debug --------------------------------------
 
 tc_debug_set_true <- function() {
