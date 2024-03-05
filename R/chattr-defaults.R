62e4812a980b649f49c2c2c2dd4074c17eec1dda
Edgar Ruiz <edgararuiz@gmail.com>
2024-03-05 16:58:38 -0600

    Makes sure yaml file is the only one checked

--- R/chattr-defaults.R
+++ R/chattr-defaults.R
@@ -71,7 +71,7 @@ chattr_defaults <- function(type = "default",
     }
 
     if (file_exists(yaml_file)) {
-      check_files <- c(yaml_file, check_files)
+      check_files <- yaml_file
     }
 
     for (j in seq_along(check_files)) {
