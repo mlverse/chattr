a233afab4da7f584aa8696381a90a7d9c4294045
Edgar Ruiz <edgararuiz@gmail.com>
2024-03-13 17:37:50 -0500

    Prompt only shows up if no model_label is passsed

--- R/chattr-use.R
+++ R/chattr-use.R
@@ -5,29 +5,31 @@
 #' R session.
 #' @export
 chattr_use <- function(model_label = NULL) {
-  if (is_interactive() && is.null(model_label)) {
+  interactive_label <- is_interactive() && is.null(model_label)
+  if (interactive_label) {
     model_label <- ch_get_ymls()
-
   }
   use_switch("configs", path_ext_set(model_label, "yml"))
-  cli_text()
-  cli_inform(
-    "Would you like to make this the default by creating a new 'chattr.yml' file in your working folder?"
+  if (interactive_label) {
+    cli_text()
+    cli_inform(
+      "Would you like to make this the default by creating a new 'chattr.yml' file in your working folder?"
     )
-  save_yaml <- menu(c("Yes", "No"))
-  if(save_yaml == 1) {
-    if(file_exists("chattr.yml")) {
-      cli_text()
-      cli_inform(
-        "'chattr.yml' already exists, replace?"
-      )
-      replace <- menu(c("Yes", "No"))
-      overwrite <- replace == 1
-      if(!overwrite) {
-        return(invisible())
+    save_yaml <- menu(c("Yes", "No"))
+    if(save_yaml == 1) {
+      if(file_exists("chattr.yml")) {
+        cli_text()
+        cli_inform(
+          "'chattr.yml' already exists, replace?"
+        )
+        replace <- menu(c("Yes", "No"))
+        overwrite <- replace == 1
+        if(!overwrite) {
+          return(invisible())
+        }
       }
+      chattr_defaults_save(overwrite = overwrite)
     }
-    chattr_defaults_save(overwrite = overwrite)
   }
 }
 
