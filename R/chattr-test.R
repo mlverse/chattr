7c65c7c591c0ff56470589d8ccbaf6dc11be57b9
Edgar Ruiz <edgararuiz@gmail.com>
2024-03-15 11:15:31 -0500

    Adds test

--- R/chattr-test.R
+++ R/chattr-test.R
@@ -80,3 +80,30 @@ ch_test.ch_llamagpt <- function(defaults = NULL) {
   }
   invisible()
 }
+
+# ----------------------------- Copilot ----------------------------------------
+
+#' @export
+ch_test.ch_openai_copilot_chat <- function(defaults = NULL) {
+  if (ch_debug_get()) {
+    prompt <- "TEST"
+    out <- "TEST"
+  } else {
+    prompt <- "Hi!"
+    out <- capture.output(chattr(prompt))
+  }
+
+  if (is.null(out)) out <- ""
+
+  cli_div(theme = cli_colors())
+  cli_h3("Testing chattr")
+  print_provider(defaults)
+
+  if (nchar(out) > 0) {
+    cli_alert_success("Connection with GitHub Copilot cofirmed")
+    cli_text("|--Prompt: {.val2 {prompt}}")
+    cli_text("|--Response: {.val1 {out}}")
+  } else {
+    cli_alert_danger("Connection with GitHub Copilot failed")
+  }
+}
