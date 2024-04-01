97261aa2d71219e7f3de05bb86e54481b6d57022
Edgar Ruiz <edgararuiz@gmail.com>
2024-04-01 14:27:34 -0500

    Updates llamagpt tests

---
+++ tests/testthat/_snaps/backend-llamagpt.md
@@ -0,0 +1,41 @@
+# Submit method works
+
+    Code
+      ch_submit(def, "test", preview = TRUE)
+    Output
+      [1] "test(Use the R language, the tidyverse, and tidymodels\n)"
+
+---
+
+    Code
+      ch_submit(def, "test", preview = TRUE)
+    Output
+      [1] "test(Use the R language, the tidyverse, and tidymodels\n)"
+
+# Printout works
+
+    Code
+      ch_llamagpt_printout(def, output = "xxx")
+    Message
+      
+      -- chattr --
+      
+      -- Initializing model 
+    Output
+      test return
+
+---
+
+    Code
+      ch_llamagpt_printout(def, output = "xxx")
+    Output
+      [1] "test return"
+
+# Args output is correct
+
+    Code
+      out
+    Output
+      [1] "--threads"   "4"           "--temp"      "0.01"        "--n_predict"
+      [6] "1000"        "--model"    
+
--- tests/testthat/test-backend-llamagpt.R
+++ tests/testthat/test-backend-llamagpt.R
@@ -1,86 +1,61 @@
-skip()
-
-test_that("Sets to LLamaGPT model", {
-  expect_snapshot(chattr_use("llamagpt"))
+test_that("Submit method works", {
+  local_mocked_bindings(
+    ch_llamagpt_output = function(...) return("test return"),
+    ch_llamagpt_session = function(...) invisible(),
+    ch_llamagpt_prompt = function(...) invisible()
+  )
+  def <- test_simulate_model("llamagpt.yml")
+  expect_equal(
+    ch_submit(def, "test"),
+    "test return"
+  )
+  expect_snapshot(
+    ch_submit(def, "test", preview = TRUE)
+  )
+  def$include_history <- FALSE
+  expect_snapshot(
+    ch_submit(def, "test", preview = TRUE)
+  )
+  expect_equal(
+    ch_submit(def, "test", preview = TRUE, prompt_build = FALSE),
+    "test"
+  )
 })
 
 test_that("Session management works", {
-  expect_false(ch_llamagpt_stop())
-
-
   x <- list()
   x$kill <- function() TRUE
   x$is_alive <- function() TRUE
   x$read_error <- function() ""
   ch_env$llamagpt$session <- x
-
-  expect_named(ch_llamagpt_session())
-  expect_snapshot(chattr_test(defaults = chattr_defaults()))
-  expect_true(ch_llamagpt_stop())
-
+  def <- test_simulate_model("llamagpt.yml")
+  temp_folder <- tempdir()
+  def$path <- temp_folder
+  def$model_arguments <- c("e" = "print('hello')")
+  expect_named(ch_llamagpt_session(defaults = def))
   ch_env$llamagpt$session <- NULL
 })
 
+test_that("Printout works", {
+  local_mocked_bindings(
+    ch_llamagpt_output = function(...) return("test return")
+  )
+  def <- test_simulate_model("llamagpt.yml")
+  expect_snapshot(
+    ch_llamagpt_printout(def, output = "xxx")
+  )
+  def$type <- "chat"
+  expect_snapshot(
+    ch_llamagpt_printout(def, output = "xxx")
+  )
+})
 
 test_that("Args output is correct", {
-  out <- ch_llamagpt_args(chattr_defaults())
+  def <- test_simulate_model("llamagpt.yml")
+  out <- ch_llamagpt_args(def)
   model_line <- grepl("ggml-gpt4all-j-v1.3-groovy.bin", out)
   # Removes expanded path because it will be different based
   # on the user who is running the tests
   out <- out[!model_line]
   expect_snapshot(out)
-})
-
-test_that("Printout works", {
-  test_chattr_type_set("console")
-  expect_snapshot(
-    ch_llamagpt_printout(chattr_defaults(), output = "xxx\n> ")
-  )
-  test_chattr_type_unset()
-})
-
-test_that("Console works", {
-  expect_snapshot(
-    capture.output(
-      ch_llamagpt_output(
-        output = "xxx\n> "
-      )
-    )
-  )
-})
-
-test_that("Chat works", {
-  chat_file <- tempfile()
-  expect_null(
-    ch_llamagpt_output(
-      output = "xxx\n> "
-    )
-  )
-  expect_equal(readRDS(chat_file), "xxx\n")
-})
-
-test_that("Submit works", {
-  local_mocked_bindings(
-    ch_llamagpt_prompt = function(...) invisible(),
-    ch_llamagpt_session = function(...) invisible(),
-    ch_llamagpt_output = function(...) {
-      return("test")
-    }
-  )
-  defaults <- yaml::read_yaml(package_file("configs", "llamagpt.yml"))
-  defaults <- as_ch_model(defaults$default, "chat")
-  expect_equal(
-    ch_submit.ch_llamagpt(
-      defaults = defaults,
-      prompt = "test",
-      stream = TRUE,
-      r_file_complete = tempfile(),
-      r_file_stream = tempfile()
-    ),
-    "test"
-  )
-})
-
-test_that("Restore to previews defaults", {
-  expect_snapshot(chattr_use("gpt35"))
 })
