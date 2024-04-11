5d6ae1ea324d251282a0e7520cac4a524d2c5758
Edgar Ruiz <edgararuiz@gmail.com>
2024-04-10 19:29:20 -0500

    Initial implementation: streamming works, it uses the REST/chat endpoint

---
+++ R/backend-ollama.R
@@ -0,0 +1,30 @@
+#' @export
+ch_submit.ch_ollama <- function(
+    defaults,
+    prompt = NULL,
+    stream = NULL,
+    prompt_build = TRUE,
+    preview = FALSE,
+    ...) {
+
+  req_body <- c(
+    list(messages = list(list(role = "user", content = prompt))),
+    model = defaults$model
+  )
+  ret <- NULL
+  req_result <- defaults$path %>%
+    request() %>%
+    req_body_json(req_body) %>%
+    req_perform_stream(
+      function(x) {
+        char_x <- rawToChar(x)
+        char_x <- jsonlite::parse_json(char_x)
+        content_x <- char_x$message$content
+        cat(content_x)
+        ret <<- paste0(ret, content_x)
+        TRUE
+      },
+      buffer_kb = 0.05, round = "line"
+    )
+  ret
+}
