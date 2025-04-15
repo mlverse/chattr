ce97b4390ae4f8270c4eca3f06799be2926aa4b4
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-04-15 14:50:39 -0500

    Expands ch_submit for ellmer

--- R/backend-ellmer.R
+++ R/backend-ellmer.R
@@ -2,8 +2,8 @@
 ch_submit.ch_ellmer <- function(
     defaults,
     prompt = NULL,
-    stream = NULL,
-    prompt_build = TRUE,
+    stream = TRUE,
+    prompt_build = FALSE,
     preview = FALSE,
     ...) {
 
@@ -11,8 +11,20 @@ ch_submit.ch_ellmer <- function(
   code_expr <- rlang::parse_expr(code_raw)
   ellmer_obj <- rlang::eval_bare(code_expr)
 
+  if (prompt_build) {
+    # re-use OpenAI prompt
+    prompt <- ch_openai_prompt(defaults, prompt)
+  }
+
+  if(preview) {
+    return(prompt)
+  }
+
   stream <- ellmer_obj$stream(prompt)
+  ret <- NULL
   coro::loop(for (chunk in stream) {
+    ret <- paste0(ret, chunk)
     cat(chunk)
   })
+  ret
 }
