931df20cae7607316220b9030977a7ae41c6a5bd
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-05-01 17:04:29 -0500

    Simplifies chattr test

--- tests/testthat/test-chattr.R
+++ tests/testthat/test-chattr.R
@@ -1,33 +1,10 @@
-skip()
 test_that("chattr() works", {
-  options("chattr-shiny-test" = FALSE)
+  chattr_use("test")
   local_mocked_bindings(
     ui_current = function(...) {
-      ""
+      "console"
     }
   )
-  test_model_backend()
-  expect_output(chattr("hello", stream = TRUE), "hello")
+  expect_output(chattr("hello"), "hello")
   expect_output(chattr("hello", stream = FALSE), "hello")
-  expect_snapshot(chattr("test", preview = TRUE))
-})
-
-test_that("External R submit works", {
-  skip_on_cran()
-  local_mocked_bindings(
-    ui_current = function(...) {
-      "script"
-    }
-  )
-  test_model_backend()
-  expect_silent(chattr("hello"))
-})
-
-test_that("Test for null output", {
-  skip_on_cran()
-  local_mocked_bindings(
-    ch_submit = function(...) NULL
-  )
-  test_model_backend()
-  expect_null(chattr("hello"))
 })
